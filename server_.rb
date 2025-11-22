a105c5717c827e812451453878b2da729c85ac33
aqwertyuiop48 <aqwertyuiop48@outlook.com>
2025-11-22 14:46:22 +0530

    C

--- server_.rb
+++ server_.rb
@@ -1,19 +1,19 @@
-# # http_server.rb
-# require 'socket'
-# server = TCPServer.new 5678
+# http_server.rb
+require 'socket'
+server = TCPServer.new 5678
  
-# while session = server.accept
-#   request = session.gets
-#   puts request
+while session = server.accept
+  request = session.gets
+  puts request
  
-#   session.print "HTTP/1.1 200\r\n" # 1
-#   session.print "Content-Type: text/html\r\n" # 2
-#   session.print("\r\n") # 3
-#   session.print(
-#     '''
-#     <h1>Hi Ruby World!</h1>
-#     '''
-#   ) #4
+  session.print "HTTP/1.1 200\r\n" # 1
+  session.print "Content-Type: text/html\r\n" # 2
+  session.print("\r\n") # 3
+  session.print(
+    '''
+    <h1>Hi Ruby World!</h1>
+    '''
+  ) #4
  
-#   session.close
-# end
+  session.close
+end
