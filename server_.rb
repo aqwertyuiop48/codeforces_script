be199a73ce8d1600d6be030c646d55bdd035bee9
aqwertyuiop48 <aqwertyuiop48@outlook.com>
2025-12-07 19:47:43 +0530

    C

--- server_.rb
+++ server_.rb
@@ -1,4 +1,4 @@
-# http_server.rb
+# server.rb
 require 'socket'
 
 port = ENV.fetch("PORT", 5678).to_i
@@ -10,13 +10,18 @@ while session = server.accept
   request = session.gets
   puts request
 
-  session.print "HTTP/1.1 200\r\n"
+  method, path, http_version = request.split(" ")
+
+  session.print "HTTP/1.1 200 OK\r\n"
   session.print "Content-Type: text/html\r\n"
   session.print "\r\n"
-    session.print(
-    '''
-    <h1>Hi Ruby World!</h1>
-    '''
-  )
+
+  # Do NOT send a body for HEAD request → Render health check
+  if method != "HEAD"
+    session.print <<~HTML
+      <h1>Hi Ruby World!</h1>
+    HTML
+  end
+
   session.close
 end
