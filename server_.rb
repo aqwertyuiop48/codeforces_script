9c79a129b70228039683fc4f70ec7504238b19eb
aqwertyuiop48 <aqwertyuiop48@outlook.com>
2025-12-07 20:18:09 +0530

    C

--- server_.rb
+++ server_.rb
@@ -1,27 +1,22 @@
 # server.rb
 require 'socket'
-
 port = ENV.fetch("PORT", 5678).to_i
 server = TCPServer.new port
-
 puts "Running on PORT #{port}"
-
 while session = server.accept
   request = session.gets
   puts request
-
-  method, path, http_version = request.split(" ")
-
+  
+  method, path, http_version = request&.split(" ") || ["GET", "/", "HTTP/1.1"]
+  
   session.print "HTTP/1.1 200 OK\r\n"
   session.print "Content-Type: text/html\r\n"
   session.print "\r\n"
-
   # Do NOT send a body for HEAD request → Render health check
   if method != "HEAD"
     session.print <<~HTML
       <h1>Hi Ruby World!</h1>
     HTML
   end
-
   session.close
 end
