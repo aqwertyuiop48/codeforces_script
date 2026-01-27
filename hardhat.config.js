c0068101956bb7ccd65a77c79425891fe2644890
aqwertyuiop48 <aqwertyuiop48@outlook.com>
2026-01-28 01:02:43 +0530

    m

--- hardhat.config.js
+++ hardhat.config.js
@@ -3,4 +3,17 @@ require("@nomicfoundation/hardhat-toolbox");
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.27",
+  networks: {
+    // This configures the node when you run 'npx hardhat node'
+    hardhat: {
+      blockGasLimit: 12000000,
+      gas: 12000000
+    },
+    // This configures the connection when you use '--network localhost'
+    localhost: {
+      url: "http://127.0.0.1:8545",
+      blockGasLimit: 12000000,
+      gas: 12000000
+    }
+  }
 };
